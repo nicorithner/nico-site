c801c4bf43f0ebc9de75533bfb6a0fd244b9644f
Nico Rithner <nicorithner@gmail.com>
2021-03-30 12:56:34 -0600

    Add initial navigation to App.js
    Change Articles for Contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Contact.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Projects} />
+        <Route path="/contact" component={Articles} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+          <Link to="/" className="item">Projects</Link>
+          <Link to="/contact" className="item">Articles</Link>
+          <Link to="/about" className="item">About</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>    
   );
 }
 
