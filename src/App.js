e3195af58c1fd8229ead6a0c6a51a8fe24c77291
Nico Rithner <nicorithner@gmail.com>
2021-03-30 20:00:22 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Projects from './Projects.js';
-import Articles from './Contact.js';
-import About from './About.js';
+import Projects from './components/projects/Projects.js';
+import Contact from './components/contact/Contact.js';
+import About from './components/about/About.js';
 import './App.css';
 
 function App() {
@@ -11,14 +11,14 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Route exact path="/" component={Projects} />
-        <Route path="/contact" component={Articles} />
+        <Route path="/contact" component={Contact} />
         <Route path="/about" component={About} />
   
         <div className="navigation">
           <img src={logo} className="logo-image" alt="Logo Image" />
           <div className="navigation-sub">
           <Link to="/" className="item">Projects</Link>
-          <Link to="/contact" className="item">Articles</Link>
+          <Link to="/contact" className="item">Contact</Link>
           <Link to="/about" className="item">About</Link>
   
           </div>
