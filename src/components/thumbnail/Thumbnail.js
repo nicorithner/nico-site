5cf0c602274525f797573265eb8ba4b36692fb11
Nico Rithner <nicorithner@gmail.com>
2021-03-31 14:07:10 -0600

    Upgrade sourcing of projects to make it dynamic

--- src/components/thumbnail/Thumbnail.js
+++ src/components/thumbnail/Thumbnail.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ProjectDetail from '../projectDetail/ProjectDetail.js';
 import '../../App.css';
 
-function Thumbnail(props) {
-  return (
-    <div className="project">
-      <Link to={props.link}>
-        <div className="project-image">
-          <img src={props.image} alt="Project Image" width="300" height="auto"/>
-        </div>
-        <div className="project-title">{props.title}</div>
-        <div className="project-category">{props.category}</div>
-            </Link>
-    </div>
+ function Thumbnail(props) {
+   return (
+    <BrowserRouter>
+     <div className="project">
+      <Route exact path= { props.link } component={ ProjectDetail } />
+       <Link to={props.link}>
+         <div className="project-image">
+          <img src={props.image} alt="Descriptive media" width="300" height="auto"/>
+         </div>
+         <div className="project-title">{props.title}</div>
+         <div className="project-category">{props.category}</div>
+             </Link>
+     </div>
+    </BrowserRouter>
   )
 }
 
 export default Thumbnail;
+
