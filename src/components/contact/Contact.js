f3fdfaa61981a87fb3477fe3f59a036ef7b8d92c
Nico Rithner <nicorithner@gmail.com>
2021-03-31 11:00:17 -0600

    Remove security risk from link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,7 +5,7 @@ function Contact(props) {
     <div>
       <h1>Contact</h1>
       <p align="center">
-        <a target="_blank"href="https://www.linkedin.com/in/nicorithner/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" alt="linkedin logo" /></a>
+        <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/nicorithner/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" alt="linkedin logo" /></a>
         <a href="mailto:nicorithner@gmail.com?subject=Hello%20Ileri,%20From%20Github"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=for-the-badge&logo=gmail&logoColor=white" alt="gmail logo"/></a>
       </p>
     </div>
