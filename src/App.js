3f67d82d3fd5edaa95991196a4f3a4ce62a91613
Nico Rithner <nicorithner@gmail.com>
2021-03-30 20:18:42 -0600

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './components/home/Home.js';
 import Projects from './components/projects/Projects.js';
 import Contact from './components/contact/Contact.js';
 import About from './components/about/About.js';
@@ -10,14 +10,15 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" component={Projects} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
         <Route path="/about" component={About} />
   
         <div className="navigation">
-          <img src={logo} className="logo-image" alt="Logo Image" />
           <div className="navigation-sub">
-          <Link to="/" className="item">Projects</Link>
+          <Link to="/" className="item">Home</Link>
+          <Link to="/projects" className="item">Projects</Link>
           <Link to="/contact" className="item">Contact</Link>
           <Link to="/about" className="item">About</Link>
   
