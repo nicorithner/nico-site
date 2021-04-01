f4d46c0ef1a57d36bcbcf596e5abb58c585e47e5
Nico Rithner <nicorithner@gmail.com>
2021-04-01 15:29:05 -0600

    Add reference to properties. Not all working yet

--- src/components/projectDetail/ProjectDetail.js
+++ src/components/projectDetail/ProjectDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function ProjectDetail(props) {
+function ProjectDetail({project}) {
   return (
-    <div className="project-container">
-      <h1>Hello World</h1>
-    
-    </div>
+  <div className="project-container">
+    <div className="heading"> <h1>{project.title}</h1> </div>
+    <div className="banner"><img src={project.banner} alt="project banner" /></div>
+    <div className="pic1"> <img src={project.pic1} alt="project display"/></div>
+    <div className="pic1"> <img src={project.pic2} alt="project display two"/></div>
+
+    <div className="text1"> <p> {project.text1} </p></div>
+    <div className="text2"> <p> {project.text1} </p></div>
+    <div className="text3"> <p> {project.text1} </p></div>
+  </div>
   );
 }
 
