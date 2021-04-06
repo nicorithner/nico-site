18665eb5fa59ad6a17442c330e30bc4b5c75243e
Nico Rithner <nicorithner@gmail.com>
2021-04-06 12:06:31 -0600

    Add styling to project detail page
    add mobile responsiveness

--- src/components/projectDetail/ProjectDetail.js
+++ src/components/projectDetail/ProjectDetail.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import './ProjectDetail.css';
 
 function ProjectDetail({project}) {
   return (
   <div className="project-container">
-    <div className="heading"> <h1>{project.title}</h1> </div>
-    <div className="banner"><img src={project.banner} alt="project banner" /></div>
-    <div className="pic1"> <img src={project.pic1} alt="project display"/></div>
-    <div className="pic1"> <img src={project.pic2} alt="project display two"/></div>
+    <section className="header">
+      <h1>{project.title}</h1>
+      <img src={project.banner} alt="project banner" />
+    </section>
 
-    <div className="text1"> <p> {project.text1} </p></div>
-    <div className="text2"> <p> {project.text1} </p></div>
-    <div className="text3"> <p> {project.text1} </p></div>
+    <section className="section-1">
+      <div className="pic"> <img src={project.pic1} alt="project display" /></div>
+      <div className="text"> <p> {project.text1} </p></div>
+    </section>
+
+    <section className="section-2">
+      <div className="text"> <p> {project.text2} </p></div>
+      <div className="pic"> <img src={project.pic1} alt="project display two" /></div>
+    </section>
+
+    <section className="section-3">
+      <div className="pic"> <img src={project.pic1} alt="project display two" /></div>
+      <div className="text"> <p> {project.text2} </p></div>
+    </section>
   </div>
   );
 }
