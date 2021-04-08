156afd59cbdc6c4f80325b4cebb90ad179f58996
Nico Rithner <nicorithner@gmail.com>
2021-04-08 11:18:27 -0600

    Replace home hero picture
    Touch up responsiveness

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,15 @@ import './Home.css'
 function Home(props) {
   return (
    <div className="hero-container">
+    
      <div className="hero-heading">
       <h1> My Software Development Journey</h1>
      </div>
-      <div className="hero-image">
-          <img src={ './images/nico-no-bg.png' } alt="Nico face" />
-      </div>
+
+     <div className="hero-image">
+        <img src={ './images/nico-no-bg1000.png' } alt="Nico face" />
+     </div>
+
    </div>
   )
 }
