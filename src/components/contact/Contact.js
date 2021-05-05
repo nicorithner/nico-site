19221564b28c3ab33aec0ad819953387b17a99a0
Nico Rithner <nicorithner@gmail.com>
2021-05-05 10:30:30 -0600

    Fix Github link
    Added Gmail mail to link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -12,7 +12,8 @@ function Contact(props) {
 
       <div className="contact-links">
         <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/nicorithner/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" alt="linkedin logo" /></a>
-        <a className="github" rel="noreferrer" target="_blank" href={props.github}><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="linkedin logo" /></a>
+        <a className="github" rel="noreferrer" target="_blank" href="https://www.github.com/nicorithner"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="github logo" /></a>
+        <a className="gmail" href="mailto:nicorithner@gmail.com?subject=Message%20From%20my%20Portfolio"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=for-the-badge&logo=gmail&logoColor=white" alt="gmail logo" /></a>
       </div>
     
     </div>
