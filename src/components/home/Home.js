00f86eb51508842e737def6501193d3456d60632
Nico Rithner <nicorithner@gmail.com>
2022-05-31 07:25:25 -0600

    Add footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,18 +4,21 @@ import About from "../about/About.js";
 
 function Home() {
   return (
-    <div className="main">
-      <div className="hero-container">
-        <div className="hero-image">
-          <img src={"./images/nico-no-bg1000.png"} alt="Nico face" />
+    <>
+      <div className="main">
+        <div className="hero-container">
+          <div className="hero-image">
+            <img src={"./images/nico-no-bg1000.png"} alt="Nico face" />
+          </div>
+          <div className="hero-heading">
+            <h1>Nico Rithner</h1>
+            <h4>Software Engineer</h4>
+          </div>
         </div>
-        <div className="hero-heading">
-          <h1>Nico Rithner</h1>
-          <h4>Software Engineer</h4>
-        </div>
+        <About />
       </div>
-      <About />
-    </div>
+      <footer className='footer--pin'></footer>
+    </>
   );
 }
 
